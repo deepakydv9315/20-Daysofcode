cf62874c7de26f7b61ef161d7e6c3824426c3cb6
Deepak <deepakydv0001@gmail.com>
2022-06-24 01:19:11 +0530

    update source code

---
+++ projects/day-2-payment-ui/app.js
@@ -0,0 +1,10 @@
+function submit(){
+    alert("You successfully placed the order😍😇 ")
+    alert("Check your mail for more info")
+    location.reload()
+}
+function cancel(){
+    alert("You canceled the order ✅");
+    alert("Must vist again")
+    location.reload()
+}
